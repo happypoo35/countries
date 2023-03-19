b43187c47185affc801ae349a9af63c0f4bab081
happypoo35 <happypoo35@gmail.com>
2023-03-19 13:17:36 +0300

    Refactor store logic to store directory and update imports accordingly

--- hooks/useAppDispatch.ts
+++ hooks/useAppDispatch.ts
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { AppDispatch } from "../rtk/store";
+import { AppDispatch } from "../store";
 
 const useAppDispatch: () => AppDispatch = useDispatch;
 
--- hooks/useAppSelector.ts
+++ hooks/useAppSelector.ts
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
-import { RootState } from "../rtk/store";
+import { RootState } from "../store";
 
 const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
